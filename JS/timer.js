09e53c10d9c35914f188216e38d0ed390e88656e
ammarsaqib <ammarr.saqib@gmail.com>
2023-10-19 18:42:59 -0700

    save

--- JS/timer.js
+++ JS/timer.js
@@ -1,24 +1,20 @@
 let timerRef = document.querySelector('#timerDisplay');
+let startButton = document.querySelector('#stq');
 let startTime;
 let pausetime = 0;
-let minutes = 0;
-let seconds = 0;
+let minutes = parseInt(localStorage.getItem("minutes")) || 0;
+let seconds = parseInt(localStorage.getItem("seconds")) || 0;
 let int = null;
 
-window.onload = function () {
-    if (int != null) {
-        clearInterval(int);
-    }
-    displayTimer();
-};
+startButton.addEventListener('click', displayTimer());
 
 function displayTimer() {
     startTime = new Date().getTime() - pausetime; // get the starting time by subtracting the elapsed paused time from the current time
 
-    stopwatchInterval = setInterval(updateStopwatch, 1000);
-    localStorage.setItem("minutes", minutes);
-    localStorage.setItem("seconds", seconds);
-    console.log(minutes + "  " + seconds)
+    stopwatchInterval = setInterval(function () {
+        updateStopwatch();
+        savetime();
+    }, 1000);
 }
 function updateStopwatch() {
     let currentTime = new Date().getTime();
@@ -26,7 +22,14 @@ function updateStopwatch() {
     seconds = Math.floor(elapsedTime / 1000) % 60; // calculate seconds
     minutes = Math.floor(elapsedTime / 1000 / 60) % 60; // calculate minutes
     let displayTime = pad(minutes) + ":" + pad(seconds);
+    console.log(localStorage.getItem("seconds"))
     timerRef.innerHTML = displayTime;
+}
+
+function savetime() {
+    localStorage.setItem("minutes", minutes);
+    localStorage.setItem("seconds", seconds);
+    // console.log(localStorage.getItem("seconds"))
 }
 
 function stopStopwatch() {
